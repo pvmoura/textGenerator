21e9941db7de06864e1853dba5b8e6aa46be6486
Pedro Moura <pmavfmoura@gmail.com>
2015-08-19 13:55:30 -0400

    added functionality for reading files, cleaning text, making a histogram, sorting words based on frequency, choosing 100 words, and formatting them for printing. tests passing for everything except printing related code

--- lib/textGenerator.rb
+++ lib/textGenerator.rb
@@ -1,6 +1,7 @@
 require "textGenerator/version"
 
 module TextGenerator
+
   def read_file_to_array(filename)
     file = File.new(filename, "r")
     arr = file.readlines.collect { |l| l.split }
@@ -26,15 +27,39 @@ module TextGenerator
   def values_to_frequency_range(data)
     sorted_vals = data.values.uniq.sort
     sum = sorted_vals.reduce { |sum, curr| sum + curr }
-    range_hash = Hash.new
+    ranges = {}
     start = 1
     sorted_vals.each do |val|
       finish = start + val - 1
       range = (start..finish)
       start = finish + 1
-      range_hash[range] = val
+      ranges[range] = val
     end
-    range_hash
-  end  
+    ranges
+  end
+
+  def generate_text (wordshash, ranges)
+    sum = wordshash.values.uniq.reduce { |sum, curr| sum + curr }
+    text = []
+    100.times do
+      num = rand(1..sum)
+      value = get_word_count_by_frequency(num, ranges)
+      words = wordshash.select { |k,v| value == v }
+      text << words.keys.sample
+    end
+    text
+  end
   
+  def wrap_line(text, line_width=80)
+    text.gsub(/(.{1,#{line_width}})(\s+|$)/, "\\1\n")
+  end
+  
+  def pretty_print (word_array, delim="\n")
+    (0..word_array.size - 1).step(10) do |index|
+      word_array[index] = word_array[index].capitalize
+      word_array[index - 1] = word_array[index - 1] + "."
+    end
+    wrap_line(word_array.join(" "), 80) 
+  end
+
 end
--- spec/textGenerator_spec.rb
+++ spec/textGenerator_spec.rb
@@ -3,7 +3,8 @@ require 'spec_helper'
 describe TextGenerator do
   let(:text_generator_class) { Class.new { include TextGenerator } }
   let(:words) { ["here", "are", "some", "words", "are", "they", "words"]}
-
+  let(:data) { {"word" => 1, "right" => 1, "said" => 2, "fred" => 10} }
+  
   subject(:tg) { text_generator_class.new }
 
 
@@ -55,6 +56,59 @@ describe TextGenerator do
     
     it "finds the correct range and returns the corresponding count" do
       expect(tg.get_word_count_by_frequency(3, hashstack)).to eq(2)
+    end
+  end
+
+  describe "#values_to_frequency_range" do
+
+    it "returns an empty hash when data is empty" do
+      frequencies = tg.values_to_frequency_range({})
+      expect(frequencies).to eq({})
+    end
+
+    it "returns a hash with range keys and count values" do
+      frequencies = tg.values_to_frequency_range(data)
+      expect(frequencies).to eq({(1..1) => 1, (2..3) => 2, (4..13) => 10} )
+    end
+  end
+
+  context "when a text is generated" do
+    let(:text_arr) { tg.generate_text(data, tg.values_to_frequency_range(data)) }
+    
+    describe "#generate_text" do
+
+      it "has 100 words" do
+        expect(text_arr.size).to eq(100)
+      end
+    end
+
+    describe "#pretty_print" do
+      let(:split_text) { tg.pretty_print(text_arr).split(".") }
+
+      it "has 10 sentences" do
+        print split_text
+        expect(split_text.size).to eq(10)
+      end
+
+      it "has sentences with 10 words" do
+        split_text.each do |sentence|
+          expect(sentence.split(" ").size).to eq(10)
+        end
+        # text_arr.each_with_index do |word, index|
+        #   expect(word).not_to be nil
+        #   if index % 10
+        #     expect(/\./.match(text_arr[index - 1])).not_to be nil
+        #   end
+        # end
+      end
+
+      it "has capitalized first words" do
+        split_text.each do |sentence|
+          words = sentence.split(" ")
+          is_upper = /[[:upper:]]/.match(words.first[0])
+          expect(is_upper).not_to be nil
+        end
+      end
     end
   end
 
